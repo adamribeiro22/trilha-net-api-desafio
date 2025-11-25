40f7115935b2e0220c6a18b5ccec751dfc7b73c3
adam <adam.ribeiro2013@gmail.com>
2025-11-25 00:21:07 -0300

    Soluções para completar o desafio de API

--- Program.cs
+++ Program.cs
@@ -5,8 +5,13 @@ using TrilhaApiDesafio.Context;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers();
 builder.Services.AddDbContext<OrganizadorContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("ConexaoPadrao"),
+        new MySqlServerVersion(new Version(8, 0, 33))
+    )
+);
 
 builder.Services.AddControllers().AddJsonOptions(options =>
     options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
